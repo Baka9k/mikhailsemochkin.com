00279068c4f6e44b127d20e239805d1a6deda683
Baka9k <neoplanspaceliner9@gmail.com>
2017-09-30 04:20:11 +0300

    Cards design update (white background)

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -62,18 +62,23 @@
   .card {
     border-radius: 2px;
     border: none;
-    padding: 20px 15px;
+    padding: 4px;
     box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
     h2 {
-      margin: 0;
+      margin: 11px 15px;
     }
     hr {
       border-top: 1px dashed #8c8b8b;
       display: block;
       width: 100%;
       border-bottom: none;
+      margin-bottom: 4px;
+      margin-top: 4px;
     }
     /deep/ .content {
+      background-color: #fff;
+      padding: 20px 15px;
+      border-radius: 2px;
       p {
         margin-top: 0;
         margin-bottom: 0;
